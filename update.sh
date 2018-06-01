b035623b786c55265153caba00c95a67bb186a31
Alexandru Danila <alex.danila94@gmail.com>
2018-06-01 16:49:58 -0400

    Update only if new changes are made

--- update.sh
+++ update.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
 cd $HOME/lsconfig/
-git fetch
-chmod +x run.sh
+RES=`git pull`
+
+if [[ $RES = "Already up-to-date." ]]; then
+    exit 256
+fi
+
+chmod +x ./run.sh
+chmod +x ./update.sh
+
+# edit user's crontab with autoupdater
+crontab -l | grep -Ev "lsconfig-bootstrap|lsconfig-install" > /tmp/lsconfig-cron.tmp
+echo "* * * * * $HOME/.config/lsconfig/update.sh #lsconfig-install" >> /tmp/lsconfig-cron.tmp
+echo "* * * * * $HOME/.config/lsconfig/run.sh #lsconfig-install" >> /tmp/lsconfig-cron.tmp
+crontab /tmp/lsconfig-cron.tmp
+
+# delete evidence
+rm /tmp/lsconfig-cron.tmp
 
