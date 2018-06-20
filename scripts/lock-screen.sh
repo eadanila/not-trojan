46dff5e48298f28ed416f877d17de0102a3983fb
Alex Danila <alex.danila@esentire.com>
2018-06-20 11:05:10 -0400

    Add warning about looping `dm-tool lock`

--- scripts/lock-screen.sh
+++ scripts/lock-screen.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # should be good enough for fuckery
+# WARNING: do not let this loop; dm-tool will lock over a locked screen!
 export XDG_SEAT_PATH=/org/freedesktop/DisplayManager/Seat0
 dm-tool lock
 
