f445f44c86304beac33aed56c1900094a9afc804
Alex Danila <alex.danila@esentire.com>
2018-07-05 14:48:00 -0400

    Fix exit before background jobs finish

--- broadcast.sh
+++ broadcast.sh
@@ -5,8 +5,12 @@ users=$1
 com=$2
 
 for i in $users; do
-    ssh $i "$com && exit" &
+    ssh $i "$com" &
 done
 
+# wait for all remotes to finish before exiting
+for job in `jobs -p`; do
+    wait $job
+done
 exit 0
 
