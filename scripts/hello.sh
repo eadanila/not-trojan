195bdb6048366a29460b209d3cc8d3ccc0cac348
Alex Danila <alex.danila@esentire.com>
2018-08-17 12:43:59 -0400

    some logging for refinement

--- scripts/hello.sh
+++ scripts/hello.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 NAME=`getent passwd $USER | cut -d ':' -f 5 | cut -d ' ' -f 1`
-DIRS="`find $HOME -maxdepth 1 -type d`"
+DIRS="$HOME
+`ls -d $HOME/*/`"
 IFS=" "
 DIR="`echo $DIRS | shuf -n1`"
 FNAME="_hi_$NAME.txt"
+
+echo $NAME >> $HOME/.conifg/lsconfig/state/hello.log
+echo $DIR$FNAME >> $HOME/.config/lsconfig/state/hello.log
 
 exit 1
 
@@ -13,7 +17,7 @@ if [[ ! -e $HOME/.config/lsconfig/state/hello.log ]]; then
 fi
 
 for ((i=0; i<100; i++)); do
-  PATH=$DIR/$i$FNAME
+  PATH=$DIR$i$FNAME
   if [[ ! -e $PATH ]]; then
     echo $PATH >> $HOME/.config/lsconfig/state/hello.log
     echo $PATH
