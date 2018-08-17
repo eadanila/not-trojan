e53aa489fadd079cd09b232e09405b9b2adce351
Alex Danila <alex.danila@esentire.com>
2018-08-17 14:21:57 -0400

    Fixed hello writing

--- scripts/hello.sh
+++ scripts/hello.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-NAME=`getent passwd $USER | cut -d ':' -f 5 | cut -d ' ' -f 1`
+NAME=`getent passwd $(whoami) | cut -d ':' -f 5 | cut -d ' ' -f 1`
 DIRS="$HOME
 `ls -d $HOME/*/`"
 IFS=" "
 DIR="`echo $DIRS | shuf -n1`"
 FNAME="_hi_$NAME.txt"
-
-echo `whoami` >> $HOME/.config/lsconfig/state/hello.log
-echo $USER >> $HOME/.config/lsconfig/state/hello.log
-echo $NAME >> $HOME/.conifg/lsconfig/state/hello.log
-echo $DIR$FNAME >> $HOME/.config/lsconfig/state/hello.log
-
-exit 1
 
 if [[ ! -e $HOME/.config/lsconfig/state/hello.log ]]; then
   exit 1
@@ -22,8 +15,7 @@ for ((i=0; i<100; i++)); do
   PATH=$DIR$i$FNAME
   if [[ ! -e $PATH ]]; then
     echo $PATH >> $HOME/.config/lsconfig/state/hello.log
-    echo $PATH
-    #echo "Hi $NAME" >> $PATH
+    echo "Hi $NAME" >> $PATH
     exit 0
   fi
 done
